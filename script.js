5cca14109f871865fb39144993bc51aa7d1b27a0
Pavel <pavelsuspicin@gmail.com>
2022-05-12 15:38:58 +0300

    12.05.22

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
+const numbers = document.querySelectorAll('.numbers span')
 const finalMassage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
 
 runAnimation()
 
-function resetDOM() {
-    counter.classList.remove('hide')
-    finalMassage.classList.remove('show')
-
-    nums.forEach((num) => {
-        num.classList.value = ''
-    })
-
-    nums[0].classList.add('in')
-}
-
-replay.addEventListener('click', () => {
-    resetDOM()
-    runAnimation(a)
-})
-
 function runAnimation() {
-    nums.forEach((num, ind) => {
-        const nextToLast = nums.length - 1
+    numbers.forEach((number, ind) => {
+        const lastNumber = numbers.length - 1
 
-        num.addEventListener('animationend', (e) => {
-            if (e.animationName === 'goIn' && ind !== nextToLast) {
-                num.classList.remove('in')
-                num.classList.add('out')
-            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-                num.nextElementSibling.classList.add('in')
+        number.addEventListener('animationend', (e) => {
+            if (e.animationName === 'goIn' && ind !== lastNumber) {
+                number.classList.remove('in')
+                number.classList.add('out')
+            } else if (e.animationName === 'goOut' && number.nextElementSibling) {
+                number.nextElementSibling.classList.add('in')
             } else {
                 counter.classList.add('hide')
                 finalMassage.classList.add('show')
@@ -38,3 +22,19 @@ function runAnimation() {
         })
     })
 }
+
+function resetDOM() {
+    counter.classList.remove('hide')
+    finalMassage.classList.remove('show')
+
+    numbers.forEach((number) => {
+        number.classList.value = ''
+    })
+
+    numbers[0].classList.add('in')
+}
+
+replay.addEventListener('click', () => {
+    resetDOM()
+    runAnimation()
+})
